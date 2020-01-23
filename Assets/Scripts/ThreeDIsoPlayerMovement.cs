65940bd53f3733a3d4421e09dff084becb0a68c2
Xiaoqi Gao <xiaoqi.gao@mail.utoronto.ca>
2020-01-23 01:20:34 -0500

    fixed the collision issue and cleaned up code in three diso player

--- Assets/Scripts/ThreeDIsoPlayerMovement.cs
+++ Assets/Scripts/ThreeDIsoPlayerMovement.cs
@@ -8,26 +8,23 @@ public class ThreeDIsoPlayerMovement : MonoBehaviour
     private float _speed = 100f;
     private float _walkSpeed = 0.5f;
     private float _runSpeed = 1f;
+
  
- 
-    private float gravity = 8;
- 
-    private Rigidbody body;
-    private Animator anim;
+    private Rigidbody _body;
  
     private Vector3 direction;
-    float percent;
  
     private void Awake()
     {
-        body = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody>();
     }
  
     private void FixedUpdate()
     {
         Move();
  
+        /// if the mc is not at the same location as the camera, 
+        /// handle camera rotation
         if (direction != Vector3.zero)
         {
            HandleRotation();
@@ -44,30 +41,31 @@ public class ThreeDIsoPlayerMovement : MonoBehaviour
         direction = new Vector3(xAxis, yAxis, zAxis);
  
         direction = direction.normalized;
+
+        /// _body.AddForce(direction * _speed);
      
-        // left controll is speed up, delete this code 
+        /// left controll is speed up, delete this code 
         if (Input.GetButton("Fire1"))
         {
-            percent = _runSpeed * direction.magnitude;
             _speed = 200f;
         }
         else
         {
-            percent = _walkSpeed * direction.magnitude;
             _speed = 100f;
         }
  
-        //CONVERT direction from local to world relative to camera
-        body.velocity = Camera.main.transform.TransformDirection(direction) * _speed * Time.deltaTime;
+        /// CONVERT direction from local to world relative to camera
+        _body.velocity = Camera.main.transform.TransformDirection(direction) * _speed * Time.deltaTime;
     }
  
     public void HandleRotation()
     {
+        /// determines which angle that the camera is looking at
         float targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
         Quaternion lookAt = Quaternion.Slerp(transform.rotation,
                                       Quaternion.Euler(0,targetRotation,0),
                                       0.5f);
-        body.rotation = lookAt;
+        _body.rotation = lookAt;
  
     }
  
