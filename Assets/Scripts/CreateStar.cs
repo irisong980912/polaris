448d9a116baef37333e96b1e0a3ba1a3121c7452
Zelin <whzss97@hotmail.com>
2020-01-23 16:40:44 -0500

    interaction kind of works

--- Assets/Scripts/CreateStar.cs
+++ Assets/Scripts/CreateStar.cs
@@ -6,14 +6,15 @@ public class CreateStar : MonoBehaviour
 {
     public GameObject star;
     public Material newTexture;
+    public bool onTrigger;
+
 
     // OnTriggerStay is called every physics update a GameObject that has a RigidBody is in the collider.
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown("e"))
         {
-            FormStar();
-            print("create" + star.name);
+            onTrigger = !onTrigger;
         }
     }
     
@@ -31,7 +32,15 @@ public class CreateStar : MonoBehaviour
         this.gameObject.GetComponent<MeshRenderer>().material = newTexture;
     }
     
-
+    private void Update()
+    {
+        if (onTrigger == true){
+            FormStar();
+            print(star.GetComponent<Gravity>().enabled);
+            print("create" + star.name);
+        }
+        
+    }
     //TODO: Consume stardust, change star material.
     
     //TODO: Later, call animation to create star, play SFX.
