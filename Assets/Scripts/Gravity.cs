4d707b53769253b68c591e2f3c2ef63b7020622b
xAmaranth <!000Floppy>
2020-01-29 16:54:25 -0500

    Modified gravity: gravity only affects objects at the edge of its range.
    Contains many assets changes. Review to ensure no critical assets are lost.

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
@@ -11,7 +8,7 @@ public class Gravity : MonoBehaviour
     private GameObject[] _gravityObjects;
     public float gravityStrength;
     public float gravityRadius;
-    
+
     private void Start()
     {
         try
@@ -24,6 +21,7 @@ public class Gravity : MonoBehaviour
                 {
                     goList.Add(go);
                 }
+
                 _gravityObjects = goList.ToArray();
             }
         }
@@ -37,14 +35,30 @@ public class Gravity : MonoBehaviour
     {
         foreach (var gravityObject in _gravityObjects)
         {
-            gravityObject.GetComponent<Rigidbody>().AddExplosionForce(
-                -gravityStrength, 
-                transform.position, 
-                gravityRadius,
-                0.0f,
-                ForceMode.Force
-                );
+            if (gameObject.tag.Contains("|Star|"))
+            {
+                if (Vector3.Distance(transform.position, gravityObject.transform.position) > 0.8 * gravityRadius)
+                {
+                    ApplyGravity(gravityObject);
+                }
+            }
+            else
+            {
+                ApplyGravity(gravityObject);
+            }
+            
         }
-        
+    }
+
+    private void ApplyGravity(GameObject gravityObject)
+    {
+        gravityObject.GetComponent<Rigidbody>().AddExplosionForce(
+            -gravityStrength,
+            transform.position,
+            gravityRadius,
+            0.0f,
+            ForceMode.Force
+        );
     }
 }
+    
