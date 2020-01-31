123776d265de468d168d27d921b97d2fc76f4032
Xiaoqi Gao <xiaoqi.gao@mail.utoronto.ca>
2020-01-30 20:09:22 -0500

    gravity script

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -2,38 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
-{
-    // Update is called once per frame
-
+@@ -11,7 +8,7 @@ public class Gravity : MonoBehaviour
     private GameObject[] _gravityObjects;
     public float gravityStrength;
     public float gravityRadius;
     
+
     private void Start()
     {
         try
-        {
-            var goList = new List<GameObject>();
-            foreach (var o in GameObject.FindObjectsOfType(typeof(GameObject)))
-            {
-                var go = (GameObject) o;
-                if (go.tag.Contains("|GravityObject|"))
+@@ -24,6 +21,7 @@ private void Start()
                 {
                     goList.Add(go);
                 }
+
                 _gravityObjects = goList.ToArray();
             }
         }
-        catch (UnityException)
-        {
-            print("No such tag");
-        }
-    }
-
-    private void Update()
+@@ -37,14 +35,30 @@ private void Update()
     {
         foreach (var gravityObject in _gravityObjects)
         {
@@ -44,7 +34,30 @@ public class Gravity : MonoBehaviour
                 0.0f,
                 ForceMode.Force
                 );
+            if (gameObject.tag.Contains("|Star|"))
+            {
+                if (Vector3.Distance(transform.position, gravityObject.transform.position) > 0.8 * gravityRadius)
+                {
+                    ApplyGravity(gravityObject);
+                }
+            }
+            else
+            {
+                ApplyGravity(gravityObject);
+            }
+
         }
-        
+
+    }
+
+    private void ApplyGravity(GameObject gravityObject)
+    {
+        gravityObject.GetComponent<Rigidbody>().AddExplosionForce(
+            -gravityStrength,
+            transform.position,
+            gravityRadius,
+            0.0f,
+            ForceMode.Force
+        );
     }
 }
