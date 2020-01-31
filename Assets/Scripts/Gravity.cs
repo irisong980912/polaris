41eb3e4fbcfdac749f8d07a14bfd160410e3805f
Xiaoqi Gao <xiaoqi.gao@mail.utoronto.ca>
2020-01-30 19:52:57 -0500

    changed gravity

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
@@ -24,6 +21,7 @@ public class Gravity : MonoBehaviour
                 {
                     goList.Add(go);
                 }
+
                 _gravityObjects = goList.ToArray();
             }
         }
@@ -37,14 +35,34 @@ public class Gravity : MonoBehaviour
     {
         foreach (var gravityObject in _gravityObjects)
         {
-            gravityObject.GetComponent<Rigidbody>().AddExplosionForce(
-                -gravityStrength, 
-                transform.position, 
-                gravityRadius,
-                0.0f,
-                ForceMode.Force
-                );
+            if (gameObject.tag.Contains("|Star|"))
+            {
+                if (Vector3.Distance(transform.position, gravityObject.transform.position) > 0.8 * gravityRadius)
+                {
+                    ApplyGravity(gravityObject);
+                }
+            }
+            else
+            {
+                ApplyGravity(gravityObject);
+            }
+            
         }
-        
+    }
+
+    private void ApplyGravity(GameObject gravityObject)
+    {
+        gravityObject.GetComponent<Rigidbody>().AddExplosionForce(
+            -gravityStrength,
+                -gravityStrength, 
+            -gravityStrength,
+            transform.position,
+                transform.position, 
+            transform.position,
+            gravityRadius,
+            0.0f,
+            ForceMode.Force
+        );
     }
 }
+    
