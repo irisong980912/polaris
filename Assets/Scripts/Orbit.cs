42fa110f232a2426b69074f3a0dee6ebc1ad5def
xAmaranth <!000Floppy>
2020-01-31 17:23:03 -0500

    Implemented orbits; slightly finicky/buggy/requires "skill".
    Tested and mostly working, does not use MC movement script for orbits.
    
    Adjusted tags to be consistent with rest of project.

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -1,5 +1,4 @@
-﻿ using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Attached to an object that would be the epicentre of the orbit,
@@ -13,17 +12,26 @@ public class Orbit : MonoBehaviour
 {
     public float speed;
     
-    /// <summary>
-    /// Rotates transform.
-    /// </summary>
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         transform.Rotate(transform.up, speed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        other.gameObject.transform.SetParent(transform);
+        if (gameObject.tag.Contains("|Star|"))
+        {
+            if (other.gameObject.tag.Contains("|Planet|"))
+            {
+                other.gameObject.transform.SetParent(transform);
+            }
+        }
+        else if (gameObject.tag.Contains("|PlanetCore|"))
+        {
+            if (!other.gameObject.tag.Contains("|Player|")) return;
+            other.transform.forward = other.transform.position - transform.position;
+            other.gameObject.transform.SetParent(transform);
+        }
     }
 
     private void OnTriggerExit(Collider other)
