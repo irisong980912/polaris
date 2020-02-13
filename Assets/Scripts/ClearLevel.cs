c1bd749efbf241a12f186d7e2f1efb7395a1c500
Iris <irisong.university@gmail.com>
2020-02-13 15:19:34 -0500

    add clear level UI image and interaction

---
+++ Assets/Scripts/ClearLevel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+using UnityEngine;
+using UnityEngine.UI;
+public class ClearLevel : MonoBehaviour
+{
+    [SerializeField] private GameObject clearLevelImage;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("|Player|"))
+        {
+            clearLevelImage.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.CompareTag("|Player|"))
+        //{
+        //    clearLevelImage.SetActive(false);
+        //}
+    }
+
+}
