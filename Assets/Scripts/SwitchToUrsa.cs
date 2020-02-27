fbc2d22eded8f3ac641ea6c4fee96767b9ba4240
Iris <irisong.university@gmail.com>
2020-02-27 16:18:55 -0500

    changed the swtich triangle

---
+++ Assets/Scripts/SwitchToUrsa.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SwitchToUrsa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame () 
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            PlayGame();
+        }
+    }
+}
