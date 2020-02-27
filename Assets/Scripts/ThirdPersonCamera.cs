f86a1be3a23ad186b4e754dce275e46da87f6d9d
xAmaranth <tse.edison@gmail.com>
2020-02-26 19:07:00 -0500

    Simplified overly complicated orbit handling.
    Naming conventions.
    
    Signed-off-by: xAmaranth <tse.edison@gmail.com>

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -2,14 +2,12 @@
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform character;          // the player to look at
-    public Transform cam;             // camera itself
-
-    private Transform _planet;
+    public Transform character;           // the player to look at
+    public Transform cam;                 // camera itself
 
     // set the min and max camera angle on X and Y axis
-    public float yAngleMin = -80.0f;  // bottom degree
-    public float yAngleMax = 80.0f;   // top degree
+    public float yAngleMin = -80.0f;      // bottom degree
+    public float yAngleMax = 80.0f;       // top degree
     public float smoothSpeed = 0.125f;
     public float distance = 0.8f;
 
@@ -18,12 +16,9 @@ public class ThirdPersonCamera : MonoBehaviour
 
     private Transform _target;
 
-    private bool _orbitDetected;
-    private bool _isCancel;
-
     private bool _levelCleared;
 
-    public Transform TopViewCamPos;
+    public Transform topViewCamPos;
 
     /// <summary>
     /// Camera Starting Position, creating a zoom in effect
@@ -31,7 +26,6 @@ public class ThirdPersonCamera : MonoBehaviour
     private void Start()
     {
         _target = character;
-        _orbitDetected = false;
 
         var dir = new Vector3(0, 0, -10.0f);
         var rotation = Quaternion.Euler(_currentY, _currentX, 0);
@@ -44,14 +38,6 @@ public class ThirdPersonCamera : MonoBehaviour
 
     private void Update()
     {
-        if (_planet)
-        {
-            print("_orbitDetected true");
-            smoothSpeed = 0.8f;
-            cam.LookAt(_planet);
-
-        }
-
         // move the camera angle by mouse
         _currentX += Input.GetAxis("Mouse X");
         _currentY += Input.GetAxis("Mouse Y");
@@ -63,33 +49,19 @@ public class ThirdPersonCamera : MonoBehaviour
 
     private void LateUpdate()
     {
-        // TODO: Test the camera smooth speed when player orbiting the planet
-        if (_planet)
-        {
-            print("_orbitDetected true");
-            smoothSpeed = 0.8f;
-            cam.LookAt(_planet);
-        }
-        else if (!_planet)
-        {
-            // set back to default
-            smoothSpeed = 0.02f;
-            cam.LookAt(_target);
-        }
-
-        //if (_levelCleared) return;
+        cam.LookAt(_target);
 
         // Camera smooth movement can be only realized in a update() function
         if (_levelCleared)
         {
-            Vector3 desiredPosition = TopViewCamPos.position;
+            var desiredPosition = topViewCamPos.position;
             transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.0125f);
 
-            Quaternion newRot = Quaternion.Euler(90, -45, -500); 
+            var newRot = Quaternion.Euler(90, -45, -500); 
             transform.rotation = Quaternion.Slerp(transform.rotation, newRot, 0.0125f);
-        } else
+        } 
+        else
         {
-
             // position the camera behind the player by "distance"
             var dir = new Vector3(0, 0, -distance);
             var rotation = Quaternion.Euler(_currentY, _currentX, 0);
@@ -101,20 +73,19 @@ public class ThirdPersonCamera : MonoBehaviour
         
     }
 
-
     private void OrbitDetected(Transform planet)
     {
-        _planet = planet;
-        _orbitDetected = true;
+        // TODO: Test the camera smooth speed when player orbiting the planet
+        smoothSpeed = 0.8f;
+        _target = planet;
         Debug.Log("OrbitDetected");
     }
 
 
     private void CancelFocus()
     {
-        _planet = null;
+        smoothSpeed = 0.02f;
         _target = character;
-        _orbitDetected = false;
         Debug.Log("CancelFocus");
     }
     
@@ -123,6 +94,5 @@ public class ThirdPersonCamera : MonoBehaviour
         _levelCleared = true;
         Debug.Log("Camera -- OnLevelClear");
     }
-
-
+    
 }
