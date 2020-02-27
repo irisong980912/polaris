3a5d0e1851a3f3743501ff4fd291b453dbebe209
xAmaranth <tse.edison@gmail.com>
2020-02-26 22:34:10 -0500

    Implemented an IsometricCamera class, used in composition.
    Contains logic to place a camera at a location with a specified direction,
    and can switch between the two different cameras.
    
    Signed-off-by: xAmaranth <tse.edison@gmail.com>

---
+++ Assets/Scripts/IsometricCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class IsometricCamera
+{
+    // The map must update playerCamera whenever the player enters or exits a puzzle area, as the perspective to
+    // return to after closing the map would change if a player is inside a puzzle area.
+    public readonly GameObject PlayerCamera;
+    public readonly GameObject IsometricCam;
+
+    public bool IsometricCameraActive;
+
+    public IsometricCamera(GameObject playerCamera, GameObject isometricCam)
+    {
+        PlayerCamera = playerCamera;
+        IsometricCam = isometricCam;
+        
+        PlayerCamera.GetComponent<Camera>().enabled = true;
+        IsometricCam.GetComponent<Camera>().enabled = false;
+    }
+
+    public void SetIsometricCameraLocation(Vector3 cameraLocation, Vector3 directionOfView)
+    {
+        IsometricCam.transform.position = cameraLocation;
+        IsometricCam.transform.forward = directionOfView;
+    }
+
+    public void SwitchPerspective()
+    {
+        if (IsometricCameraActive)
+        {
+            IsometricCameraActive = false;
+            PlayerCamera.GetComponent<Camera>().enabled = true;
+            IsometricCam.GetComponent<Camera>().enabled = false;
+        }
+        else
+        {
+            IsometricCameraActive = true;
+            PlayerCamera.GetComponent<Camera>().enabled = false;
+            IsometricCam.GetComponent<Camera>().enabled = true;
+        }
+        
+    }
+
+    public GameObject GetActiveCamera()
+    {
+        return IsometricCameraActive ? IsometricCam : PlayerCamera;
+    }
+    
+}
