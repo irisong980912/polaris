84b8b8e7ecaf4fdf6e52687df7f1306e4c3caa91
Iris <irisong.university@gmail.com>
2020-03-01 10:45:00 -0500

    added back the player handle rotation function

--- Assets/Scripts/ThirdPersonPlayer.cs
+++ Assets/Scripts/ThirdPersonPlayer.cs
@@ -28,6 +28,7 @@ public class ThirdPersonPlayer : MonoBehaviour
         CameraSwitch.OnMapSwitch += SetMapActive;
     }
 
+    
     private static void SetMapActive(bool mapActive)
     {
         _mapActive = mapActive;
@@ -40,6 +41,7 @@ public class ThirdPersonPlayer : MonoBehaviour
         collectDustSound = collectDustSoundContainer.GetComponent<AudioSource>();
     }
 
+    
     public void OnTriggerEnter(Collider collision)
     {
         // TODO: Move me to the collect stardust script.
@@ -49,19 +51,52 @@ public class ThirdPersonPlayer : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// FixedUpdate is called for every physics frame (when player is moving)
+    /// </summary>
     private void FixedUpdate()
     {
         if (_mapActive) return;
+        Move();
+
+        // handle player rotation for movement if the player is not at the same location as the camera,
+        if (_direction != Vector3.zero)
+        {
+            HandleRotation();
+        }
+        // TODO: add the UI for stardust count back to canvas
+        //stardustcount.text = "Stardust: " + stardust;
+    }
+
+    
+    private void Move()
+    {
+        // x is left and right
+        // y is in and out
         var xAxis = Input.GetAxisRaw("Horizontal");
         var yAxis = Input.GetAxisRaw("Vertical");
 
-        _direction = new Vector3(xAxis, 0f, yAxis);
-        _direction = _direction.normalized;
+        _direction = new Vector3(xAxis, 0f, yAxis).normalized;
 
+        // Convert direction from local to world relative to camera
         _body.transform.Translate(cam.transform.TransformDirection(_direction) * speed, Space.World);
 
-        // stardustcount.text = "Stardust: " + stardust;
+    }
+
+    
+    /// <summary>
+    /// Rotate the player to face the direction of their movement.
+    /// This function will be called only when player is moving.
+    /// </summary>
+    private void HandleRotation()
+    {
+        // determines which angle that the camera is facing
+        var targetRotation = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+        // determines which angle that player should be facing
+        var lookAt = Quaternion.Slerp(transform.rotation,
+            Quaternion.Euler(0, targetRotation, 0),
+            0.5f);
+        _body.rotation = lookAt;
     }
     
 }
