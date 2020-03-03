3752261293b535ec7690c75baddc9c66cf332170
ZhangZelin <whzss97@hotmail.com>
2020-03-02 19:30:20 -0500

    Player indicator on map working

---
+++ Assets/Scripts/MapPlayerPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPlayerPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+    
+    //Update mapPlayerPosition with player position
+    private void FollowPlayer()
+    {
+        var player = GameObject.FindWithTag("|Player|").transform;
+        transform.position = new Vector3(player.position.x, player.position.y, player.position.z);
+    }
+}
