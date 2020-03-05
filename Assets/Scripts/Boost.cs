c3ea7e3ec4c3ae63e7fb81bb2a77cdd36c453b41
ZhangZelin <whzss97@hotmail.com>
2020-03-04 22:57:29 -0500

    added conditional stardust boost

---
+++ Assets/Scripts/Boost.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var playerScript = GameObject.FindWithTag("|Player|").GetComponent<ThirdPersonPlayer>();
+        if (playerScript.stardust >= 3)
+        {
+            playerScript.speed = Input.GetButton("Jump") ? 1.3f : 0.6f;
+        }
+    }
+}
