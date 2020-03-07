62a56cac35ad3b8fb0124f962a08daaeb4d67fdb
MaddieFish <MaddieFish@users.noreply.github.com>
2020-03-07 14:29:19 -0500

    Added multiple input types for DPAD
    Created a ControllerInput script to manage different controllers.

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+
+    public string[] controllers;
+
+    float xDpadAxisInput;
+    float yDpadAxisInput;
+
+    float xLAxisInput;
+    float yLAxisInput;
+
+    float xRAxisInput;
+    float yRAxisInput;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        controllers = Input.GetJoystickNames();
+  
+       //Prints Joystick values
+
+        if (controllers[0] == "")
+        {
+
+        }
+
+        xDpadAxisInput = Input.GetAxisRaw("XBOXDpadHorizontal");
+        yDpadAxisInput = Input.GetAxisRaw("XBOXDpadVertical");
+
+        xLAxisInput = Input.GetAxisRaw("Horizontal");
+        yLAxisInput = Input.GetAxisRaw("Vertical");
+
+        xRAxisInput = Input.GetAxisRaw("rHorizontal");
+        yRAxisInput = Input.GetAxisRaw("rVertical");
+
+        Debug.Log("Dpad: " + xDpadAxisInput + ", " + yDpadAxisInput);
+        Debug.Log("JoystickL: " + xLAxisInput + ", " + yLAxisInput);
+        Debug.Log("JoystickR: " + xRAxisInput + ", " + yRAxisInput);
+    }
+}
